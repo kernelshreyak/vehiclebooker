5396178ca234f2c9ab98c7cffcd3c3e6a29c5163
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2020-11-28 13:18:42 +0530

    added admin panel theme

---
+++ backend/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.admin')
+@section('title', 'Dashboard - Vehicle Booker')
+
+@section('dashboard')
+
+ <div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid">
+            <h1 class="mt-4">Dashboard</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="row">
+            </div>
+        </div>
+    </main>
+</div>
+@stop
+            
